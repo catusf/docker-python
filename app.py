ec986b5af527f0453ecf922737b67d0203e64e1f
Dan Phan <Dan Phan>
2016-08-18 10:40:12 +0700

    Change Hello message

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ start = int(round(time.time()))
 @app.route("/")
 def hello_world():
 
-    return r"<h1>Hello Tho!</h1>"
+    return r"<h1>Hello Quyên!</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=False)
--- r.sh
+++ r.sh
@@ -1,5 +1,5 @@
-docker stop webapp
-docker rm webapp
+docker stop docker-python
+docker rm docker-python
 docker run -v /c/Users/Danpm/docker/catusf/webapp/src:/opt/webapp -p 5000:5000 --name docker-python catusf/docker-python
 
 #  -v /c/Users/Danpm/docker/catusf/webapp/src:/opt/webapp
